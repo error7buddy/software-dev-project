6c11044c03109aa2249f565872f85aa96d404d44
error7buddy <fxosman7@gmail.com>
2024-01-04 12:16:30 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+
+const accessKey = "B9HL_Na0AcN79pm0hIRAgcdgy_PWbLazrcpJClUUVR8";
+const fromEl = document.querySelector("form"); 
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData=" ";
+let page = 1;
+
+async function searchImages(){
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+    const response = await fetch (url);
+    const data = await response.json();
+
+    const results = data.results;
+
+
+    if(page===1){
+        searchResults.innerHTML=" ";
+    }
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("searchResult");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        
+    
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+      });
+      page++;
+      if (page > 1) {
+        showMore.style.display = "block";
+      }
+}
+
+fromEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page=1;
+    searchImages();
+
+});
+
+showMore.addEventListener("click",()=>{
+
+    searchImages();
+
+});
---
+++ welcome.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- <link rel="stylesheet" href="style.css"> -->
+  </head>
+  <body class="bg-[url('https://i.ibb.co/4TdjwJw/Untitled-1.png')] bg-cover">
+
+      <!-- sign in sign up -->
+      <div class="text-right px-12  py-3 text-[#FFF] text-xl ">
+        <a href="register.php">
+          <button class="mr-5" >Sign Up</button></a>
+          <button class="mr-5">|</button>
+          <a href="login.php">
+          <button class="mr-5">Login</button></a>
+          <button class="mr-5">|</button>
+          <a href="home.php">
+          <button class="mr-5">profile</button></a>
+      </div>
+      
+<!-- sign in sign up -->
+    <!-- Heading -->
+    <a href="index.html">
+    <h1 class="text-6xl font-bold my-5 text-center font-sans text-[#FFF] ">
+      FoUnD
+    </h1></a>
+
+    <!-- search section -->
+
+    <form
+      class="flex justify-center items-center my-5 h-[100px] gap-2"
+      action=""
+    >
+      <input
+        class="w-[350px] h-[40px] rounded p-5 text-[#333] shadow-gray-100"
+        type="text"
+        id="search-input"
+        placeholder="Search for images"
+      />
+      <button
+        class="w-[100px] h-[40px] rounded bg-[#3260a8] text-[#FFF]"
+        id="search-button"
+      >
+        Search
+      </button>
+    </form>
+
+    <!-- main div section -->
+
+    <div class="search-results flex flex-wrap justify-around max-w-[1450px] mb-10 gap-2 ">
+
+        <!-- first div -->
+      <div class="search-result  w-[20%] h-[350px] rounded-lg bg-zinc-300">
+       
+        <img
+        class="img w-[100%] h-[200px]  mx-auto rounded-lg"
+        src="https://i.ibb.co/WFpJHmR/cat.jpg"
+        alt="Mountains under the sky"/>
+
+      <h1 class="text-center font-medium text-base mt-5 text-[#040238]  h-[50px]">Black and White cat lying on brown bamboo chair inside room</h1>
+      <a
+        href="https://unsplash.com/photos/black-and-white-cat-lying-on-brown-bamboo-chair-inside-room-gKXKBY-C-Dk"
+        target="_blank"
+        >
+        <button class="w-[110px] h-[35px] bg-[#3260a8] ml-20 mt-2  rounded-lg text-[#FFF]">View Details</button>
+        </a
+      >
+      
+       
+      </div>
+      <div class="search-result  w-[20%] h-[350px] rounded-lg bg-zinc-300">
+       
+        <img
+        class="img w-[100%] h-[200px]  mx-auto rounded-lg"
+        src="https://i.ibb.co/WFpJHmR/cat.jpg"
+        alt="Mountains under the sky"/>
+
+      <h1 class="text-center font-medium text-base mt-5 text-[#040238]  h-[50px]">Black and White cat lying on brown bamboo chair inside room</h1>
+      <a
+        href="https://unsplash.com/photos/black-and-white-cat-lying-on-brown-bamboo-chair-inside-room-gKXKBY-C-Dk"
+        target="_blank"
+        >
+        <button class="w-[110px] h-[35px] bg-[#3260a8] ml-20 mt-2 rounded-lg text-[#FFF]">View Details</button>
+        </a
+      >
+      
+       
+      </div>
+      <div class="search-result  w-[20%] h-[350px] rounded-lg bg-zinc-300">
+       
+        <img
+        class="img w-[100%] h-[200px]  mx-auto rounded-lg"
+        src="https://i.ibb.co/WFpJHmR/cat.jpg"
+        alt="Mountains under the sky"/>
+
+      <h1 class="text-center font-medium text-base mt-5 text-[#040238]  h-[50px]">Black and White cat lying on brown bamboo chair inside room</h1>
+      <a
+        href="https://unsplash.com/photos/black-and-white-cat-lying-on-brown-bamboo-chair-inside-room-gKXKBY-C-Dk"
+        target="_blank"
+        >
+        <button class="w-[110px] h-[35px] bg-[#3260a8] ml-20 mt-2 rounded-lg text-[#FFF]">View Details</button>
+        </a
+      >
+      
+       
+      </div>
+     
+      
+        <!-- second div -->
+      <!-- <div class="search-result w-[270px] h-[315px] rounded-lg bg-zinc-300">
+       
+        <img
+        class="img w-[270px] h-[180px] mx-auto rounded-lg"
+        src="https://i.ibb.co/8rJBfkk/dogss.jpg"
+        alt="Mountains under the sky"
+      />
+      <h1 class="text-center font-medium text-base mt-5 text-[#040238] h-[50px]">Black and White short coated dog</h1>
+      <a
+        href="https://unsplash.com/photos/black-and-white-short-coated-dog-N04FIfHhv_k"
+        target="_blank"
+        >
+        <button class="w-[110px] h-[35px] bg-[#3260a8] ml-20 mt-2 rounded-lg text-[#FFF]">View Details</button>
+        </a
+      >
+      
+       
+      </div> -->
+        <!-- third div -->
+      <!-- <div class="search-result w-[270px] h-[315px] rounded-lg bg-zinc-300">
+       
+        <img
+        class="img w-[270px] h-[180px] mx-auto rounded-lg"
+        src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*"
+        alt="Mountains under the sky"
+      />
+      <h1 class="text-center font-medium text-base mt-5 text-[#040238] h-[50px]">Black and White cat lying on brown bamboo chair inside room</h1>
+      <a
+        href="https://unsplash.com/photos/black-and-white-cat-lying-on-brown-bamboo-chair-inside-room-gKXKBY-C-Dk"
+        target="_blank"
+        >
+        <button class="w-[110px] h-[35px] bg-[#3260a8] ml-20 mt-2 rounded-lg text-[#FFF]">View Details</button>
+        </a
+      >
+      
+       
+      </div> -->
+        <!-- fourth div -->
+      <!-- <div class="search-result w-[270px] h-[315px] rounded-lg bg-zinc-300">
+       
+        <img
+        class="img w-[270px] h-[180px] mx-auto rounded-lg"
+        src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*"
+        alt="Mountains under the sky"
+      />
+      <h1 class="text-center font-medium text-base mt-5 text-[#040238] h-[50px]">Black and White cat lying on brown bamboo chair inside room</h1>
+      <a
+        href="https://unsplash.com/photos/black-and-white-cat-lying-on-brown-bamboo-chair-inside-room-gKXKBY-C-Dk"
+        target="_blank"
+        >
+        <button class="w-[110px] h-[35px] bg-[#3260a8] ml-20 mt-2 rounded-lg text-[#FFF]">View Details</button>
+        </a
+      >
+      
+       
+      </div> -->
+        <!-- fifth div -->
+      <!-- <div class="search-result w-[270px] h-[320px] rounded-lg bg-zinc-300">
+       
+        <img
+        class="img w-[270px] h-[180px] mx-auto rounded-lg"
+        src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*"
+        alt="Mountains under the sky"
+      />
+      <h1 class="text-center font-medium text-base mt-5 text-[#040238] h-[50px]">Black and White cat lying on brown bamboo chair inside room</h1>
+      <a
+        href="https://unsplash.com/photos/black-and-white-cat-lying-on-brown-bamboo-chair-inside-room-gKXKBY-C-Dk"
+        target="_blank"
+        >
+        <button class="w-[110px] h-[35px] bg-[#3260a8] ml-20 mt-2 rounded-lg text-[#FFF]">View Details</button>
+        </a
+      >
+      
+       
+      </div> -->
+      
+    
+    </div>
+
+    <button class="hidden w-[100px] h-[40px] rounded bg-[#3260a8] text-[#FFF] my-12 mx-auto" id="show-more-button">Show More</button>
+
+    <script src="script.js"></script>
+  </body>
+</html>
